c3a6948cae0e3fa30cbe53051398a28cd8e92a6b
RapidDab <57857061+RapidDab@users.noreply.github.com>
2024-02-06 01:17:18 +0000

    Adding the Project

---
+++ My project (25)/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    // After this time, the object will be destroyed
+
+    [Header("Default Desrtuction Time")]
+    public float timeToDesrtuction;
+
+    void Start()
+    {
+        Invoke("DestroyObject", timeToDesrtuction);
+    }
+
+    // Update is called once per frame
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
+
